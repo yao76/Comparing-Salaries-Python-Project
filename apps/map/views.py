2a4262c1f74cac3acaa5d22390a6e331ca356c2d
eric fang <ericfang1992@gmail.com>
2019-07-24 18:48:53 -0700

    committing before pulling json's

--- apps/map/views.py
+++ apps/map/views.py
@@ -18,12 +18,9 @@ def index(request):
     for col in df.columns:
         df.loc[col] = df[col].astype(str)
 
-    # x = "State: " + df['ST'] + '<br>' + 'Job Title: ' + \
-    #     df['OCC_TITLE'] + '<br>' + 'Annual Sal: ' + '$' + df['A_MEAN']
+    x = "State: " + df['ST'] + '<br>' + 'Job Title: ' + \
+        df['OCC_TITLE'] + '<br>' + 'Annual Sal: ' + '$' + df['A_MEAN']
 
-    # df_year['text'] = df_year['STATE'] + '<br>' + df_year['ST'] + '<br>'  + ' Job ' + '<br>' + df_year['OCC_TITLE'] + '<br>' + df_year['A_MEAN']
-    # 'Fruits ' + df_year['total fruits'] + ' Veggies ' + df_year['total veggies'] + '<br>' + \
-    # 'Wheat ' + df_year['wheat'] + ' Corn ' + df_year['corn']
 
     fig = go.Figure(data=go.Choropleth(
         locations=df['ST'],
@@ -31,7 +28,7 @@ def index(request):
         locationmode='USA-states',
         colorscale="greens",
         autocolorscale=False,
-        # text=x,  # hover text
+        text=x,  # hover text
         marker_line_color='white',  # line markers between states
         colorbar_title="USD"
     ))
